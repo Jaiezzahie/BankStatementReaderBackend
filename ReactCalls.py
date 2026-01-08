c6308e86f6dfbd6a720eed636d16a3488400c376
Jaiezzahie <erinjaiah@gmail.com>
2026-01-08 20:26:39 +0000

    Add note and set port to always be 5000

--- ReactCalls.py
+++ ReactCalls.py
@@ -89,4 +89,6 @@ def hsbc(file_path):
 
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(debug=True, port=5000)
+
+# Use gunicorn -b 127.0.0.1:5000 ReactCalls:app to run in production
